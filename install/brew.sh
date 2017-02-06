12701279921c98bab7663a73930013af3d326326
Ludvig Lundgren <hello@ludviglundgren.se>
2017-02-06 22:38:07 +0100

    Removed Dashlane. Updates was weird.

--- install/brew.sh
+++ install/brew.sh
@@ -25,32 +25,31 @@ formulas=(
     zsh
     z
     Caskroom/cask/iterm2
-    #Caskroom/cask/macdown
-    #Caskroom/cask/spectacle
-    #Caskroom/cask/flux
-    #Caskroom/cask/caffeine
-    #Caskroom/cask/bartender
-    #Caskroom/cask/dropbox
-    #Caskroom/cask/virtualbox
-    #Caskroom/cask/transmission
-    #Caskroom/cask/dashlane
-    #Caskroom/cask/imageoptim
-    #Caskroom/cask/google-chrome
-    #Caskroom/cask/cyberduck
-    #Caskroom/cask/mountain-duck
-    #Caskroom/cask/skype
-    #Caskroom/cask/spotify
-    #Caskroom/cask/slack
+    Caskroom/cask/macdown
+    Caskroom/cask/spectacle
+    Caskroom/cask/flux
+    Caskroom/cask/caffeine
+    Caskroom/cask/bartender
+    Caskroom/cask/dropbox
+    Caskroom/cask/virtualbox
+    Caskroom/cask/transmission
+    Caskroom/cask/imageoptim
+    Caskroom/cask/google-chrome
+    Caskroom/cask/cyberduck
+    Caskroom/cask/mountain-duck
+    Caskroom/cask/skype
+    Caskroom/cask/spotify
+    Caskroom/cask/slack
     Caskroom/cask/the-unarchiver
-    #Caskroom/cask/vlc
-    #Caskroom/cask/visual-studio-code
-    #Caskroom/cask/goofy
-    #Caskroom/cask/clipmenu
-    #Caskroom/cask/gitkraken
-    #Caskroom/cask/sequel-pro
-    #Caskroom/cask/postman
+    Caskroom/cask/vlc
+    Caskroom/cask/visual-studio-code
+    Caskroom/cask/goofy
+    Caskroom/cask/clipmenu
+    Caskroom/cask/gitkraken
+    Caskroom/cask/sequel-pro
+    Caskroom/cask/postman
     Caskroom/cask/sublime-text
-    #Caskroom/cask/webtorrent
+    Caskroom/cask/webtorrent
     Caskroom/cask/ealeksandrov-cd-to
 )
 
