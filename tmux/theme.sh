79281619c2cd57c21416985480458a87bffe662e
Ludvig Lundgren <hello@ludviglundgren.se>
2017-07-24 23:49:41 +0200

    Updated theme

--- tmux/theme.sh
+++ tmux/theme.sh
@@ -52,11 +52,9 @@ set-window-option -g clock-mode-colour $tm_color_active
 tm_tunes="#[fg=$tm_color_music]#(osascript ~/.dotfiles/applescripts/tunes.scpt)"
 tm_battery="#(~/.dotfiles/bin/battery)"
 
-tm_battery_2="#(~/.dotfiles/bin/battery_2)"
-
 tm_date="#[fg=$tm_color_inactive] %a %d %b %R"
 tm_host="#[fg=$tm_color_feature,bold]#h"
 tm_session_name="#[fg=$tm_color_feature,bold]$tm_icon #S"
 
 set -g status-left $tm_session_name' '
-set -g status-right $tm_tunes' '$tm_battery_2' '$tm_battery' '$tm_date' '$tm_host
+set -g status-right $tm_tunes' '$tm_battery' '$tm_date' '$tm_host
